41b355acaaa80f26e5e53aa9e1252089998e8a86
Eileen Ho <eileenho25@gmail.com>
2016-11-17 22:30:00 -0800

    add map class

--- w1d5/diyadt.rb
+++ w1d5/diyadt.rb
@@ -37,3 +37,33 @@ class Queue
     @queue
   end
 end
+
+class Map
+  attr_accessor :map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    if @map.empty?
+      @map << [key, value]
+    else
+      @map.each do |pair|
+        if pair[0] == key
+          pair[1] = value
+        else
+          @map << [key, value]
+        end
+      end
+    end
+  end
+
+  def lookup(key)
+    @map.select { |pair| pair[0] == key }
+  end
+
+  def remove(key)
+    @map.reject! { |pair| pair[0] == key }
+  end
+end
