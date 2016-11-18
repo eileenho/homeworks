1c8e48af5021ca6bb0b6d7d540f59da3ba6b24cf
Eileen Ho <eileenho25@gmail.com>
2016-11-17 22:01:17 -0800

    add stack methods

--- w1d5/diyadt.rb
+++ w1d5/diyadt.rb
@@ -1,13 +1,19 @@
 class Stack
+  attr_accessor :stack
+
   def initialize
+    @stack = []
   end
 
   def add(el)
+    @stack << el
   end
 
   def remove
+    @stack.pop
   end
 
   def show
+    @stack
   end
 end
