67d2ab6da8f931411030442d333b4e374cd818bf
Eileen Ho <eileenho25@gmail.com>
2016-12-07 00:11:21 -0800

    add validations to user model

--- week4/w4d3_rails_auth/AppName/app/models/user.rb
+++ week4/w4d3_rails_auth/AppName/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  before_validation :ensure_session_token
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  private
+
+    def ensure_session_token
+      self.session_token ||= SecureRandom::urlsafe_base64(16)
+    end
 end
