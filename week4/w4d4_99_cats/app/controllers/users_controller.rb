b97b6ba5530424a462b80081f674607515e349b1
Eileen Ho <eileenho25@gmail.com>
2016-12-08 19:41:59 -0800

    add usermailer

--- week4/w4d4_99_cats/app/controllers/users_controller.rb
+++ week4/w4d4_99_cats/app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      msg = UserMailer.welcome_email(@user)
+      msg.deliver
       log_in(user)
       redirect_to cats_url
     else
