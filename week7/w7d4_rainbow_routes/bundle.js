53e4713b382ed3f726a7a3365e874daf0ac58ad1
Eileen Ho <eileenho25@gmail.com>
2017-01-04 23:11:52 -0800

    add rainbow routes

--- week7/w7d4_rainbow_routes/bundle.js
+++ week7/w7d4_rainbow_routes/bundle.js
@@ -150,36 +150,46 @@
 	  }, {
 	    key: 'addRed',
 	    value: function addRed() {
-	      // your code here
+	      this.props.router.push('/red');
 	    }
 	  }, {
 	    key: 'addGreen',
 	    value: function addGreen() {
-	      // your code here
+	      this.props.router.push('/green');
 	    }
 	  }, {
 	    key: 'addBlue',
 	    value: function addBlue() {
-	      // your code here
+	      this.props.router.push('/blue');
 	    }
 	  }, {
 	    key: 'addViolet',
 	    value: function addViolet() {
-	      // your code here
+	      this.props.router.push('/violet');
 	    }
 	  }]);
 	
 	  return Rainbow;
 	}(_react2.default.Component);
 	
-	;
-	
 	Rainbow = (0, _reactRouter.withRouter)(Rainbow);
 	
 	var routes = _react2.default.createElement(
 	  _reactRouter.Route,
 	  { path: '/', component: Rainbow },
-	  '// your routes here'
+	  _react2.default.createElement(
+	    _reactRouter.Route,
+	    { path: 'red', component: _red2.default },
+	    _react2.default.createElement(_reactRouter.Route, { path: 'orange', component: _orange2.default }),
+	    _react2.default.createElement(_reactRouter.Route, { path: 'yellow', component: _yellow2.default })
+	  ),
+	  _react2.default.createElement(
+	    _reactRouter.Route,
+	    { path: 'blue', component: _blue2.default },
+	    _react2.default.createElement(_reactRouter.Route, { path: 'indigo', component: _indigo2.default })
+	  ),
+	  _react2.default.createElement(_reactRouter.Route, { path: 'green', component: _green2.default }),
+	  _react2.default.createElement(_reactRouter.Route, { path: 'violet', component: _violet2.default })
 	);
 	
 	document.addEventListener("DOMContentLoaded", function () {
@@ -25604,24 +25614,22 @@
 	  }, {
 	    key: 'resetRed',
 	    value: function resetRed() {
-	      // your code here
+	      this.props.router.push('/red');
 	    }
 	  }, {
 	    key: 'addOrange',
 	    value: function addOrange() {
-	      // your code here
+	      this.props.router.push('/red/orange');
 	    }
 	  }, {
 	    key: 'addYellow',
 	    value: function addYellow() {
-	      // your code here
+	      this.props.router.push('/red/yellow');
 	    }
 	  }]);
 	
 	  return Red;
 	}(_react2.default.Component);
-	
-	;
 	
 	exports.default = (0, _reactRouter.withRouter)(Red);
 
@@ -25765,19 +25773,17 @@
 	  }, {
 	    key: 'resetBlue',
 	    value: function resetBlue() {
-	      // your code here
+	      this.props.router.push('/blue');
 	    }
 	  }, {
 	    key: 'addIndigo',
 	    value: function addIndigo() {
-	      // your code here
+	      this.props.router.push('/blue/indigo');
 	    }
 	  }]);
 	
 	  return Blue;
 	}(_react2.default.Component);
-	
-	;
 	
 	exports.default = (0, _reactRouter.withRouter)(Blue);
 
